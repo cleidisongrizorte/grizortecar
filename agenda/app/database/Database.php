5cfb9a6bf4724081263d8a316d18972e9e93bd5b
cleidisongrizorte <cleidisongri@hotmail.com>
2024-12-11 22:02:03 -0300

    criação do metodo connect e closeConnet

--- agenda/app/database/Database.php
+++ agenda/app/database/Database.php
@@ -7,7 +7,7 @@ class Database {
     private $user;
     private $password;
     private $dbname;
-    private $conn;
+    private $connection;
 
     // Método construtor
 
@@ -17,6 +17,20 @@ class Database {
         $this->user = DB_USER;
         $this->password = DB_PASSWORD;
         $this->dbname = DB_NAME;
+    }
+
+    // Método para criar uma conexão com o banco de dados
+    public function connect(){
+        $this->connection = new mysqli($this->servername,$this->user,$this->password,$this->dbname);
+        return $this->connection$connection;
+    }
+    // Método para fechar a conexão com o banco de dados
+    public function closeConnection()
+    {
+        if ($this->connection){
+            $this->connection->close();
+        }
+
     }
 
 }
