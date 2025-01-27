0a843ce06589424b76ea8c56e2106950893610da
Cleidison Grizorte <130578954+cleidisongrizorte@users.noreply.github.com>
2025-01-27 20:36:42 -0300

    aula 27-01

--- agenda/app/cliente/ClienteControl.php
+++ agenda/app/cliente/ClienteControl.php
@@ -1,39 +1,11 @@
-<?php
-
-include_once __DIR__ . '/ClienteView.php';
-class ClienteControl{
-    // Metodo que trata a requisição do usuario
-
-    public function handleRequest($action, $params){
-        
-    //verifica se requisição e post e se existe ação
-
-    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-        switch ($action){
-            case 'cadastrar':
-            
-                
-            case 'atualizar':    
-        }
-        else
-        {
-            switch($action){
-                case 'listar':
-
-                case 'novo':
-                    // exibir formulario de cadastro de cliente
-                    $view = new ClienteView();
-                    $view->exibirFormularioCadastro();
-
-                case 'atualizar':
-                
-                case 'apagar':
-
-
-            }
-        }
-
-    }
-    }
-}
-?>
+tRightPos="1129" ExpandedHeight="0" TopContaner="26"/>
+			<Pane-26 Type="2" DockingCX="220" DockingCY="150" Horiz="1" Panes="1" Pane-1="27"/>
+			<Pane-27 Type="1" DockingCX="220" DockingCY="150" Panes="0"/>
+			<Pane-28 Type="2" DockingCX="1910" Panes="2" Pane-1="29" Pane-2="45"/>
+			<Pane-29 Type="2" DockingCX="1910" Horiz="1" Panes="2" Pane-1="30" Pane-2="44"/>
+			<Pane-30 Type="2" DockingCX="1584" Panes="1" Pane-1="31"/>
+			<Pane-31 Type="2" DockingCX="1889" DockingCY="883" Horiz="1" Panes="4" Pane-1="32" Pane-2="35" Pane-3="42" Pane-4="43"/>
+			<Pane-32 Type="2" DockingCX="330" DockingCY="828" Panes="2" Pane-1="33" Pane-2="34"/>
+			<Pane-33 Type="1" DockingCX="220" DockingCY="572" Panes="4" Selected="1" Pane-1="1" Pane-2="8" Pane-3="3" Pane-4="6"/>
+			<Pane-34 Type="1" DockingCX="220" DockingCY="252" Panes="1" Selected="2" Pane-1="2"/>
+			
--- dados.php
+++ dados.php
@@ -1,6 +1,30 @@
-<?php
+<?php 
+include_once "config.php"; 
 
-$nome = $_POST['nome']; 
-$email = $_POST['email'];
-$senha = $_POST['senha'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nome = mysqli_real_escape_string($conn, $_POST['nome']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $senha = mysqli_real_escape_string($conn, $_POST['senha']);
+    
+    // Usando Prepared Statements para segurança
+    $sql = "INSERT INTO cliente (nome, email, senha) VALUES (?, ?, ?)";
+    
+    if ($stmt = mysqli_prepare($conn, $sql)) {
+        mysqli_stmt_bind_param($stmt, "sss", $nome, $email, $senha);
+        
+        if (mysqli_stmt_execute($stmt)) {
+            header("Location: loginaula.php");
+            exit();
+        } else {
+            echo "Erro ao executar a query: " . mysqli_error($conn);
+        }
+        
+        mysqli_stmt_close($stmt);
+    } else {
+        echo "Erro ao preparar a query: " . mysqli_error($conn);
+    }
+    
+    mysqli_close($conn);
+}
 ?>
+
