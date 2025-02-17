7d02d2882762b4b4a62bf72c108f0363171196a7
cleidisongrizorte <cleidisongri@hotmail.com>
2025-02-17 11:30:07 -0300

    editando

--- agenda/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Configuraçoes de acesso ao banco de dados
-
-define('DB_HOST','localhost');
-define('DB_USER','grizorte');
-define('DB_PASSWORD','81654325');
-define('DB_NAME','agenda');
-
-?>
